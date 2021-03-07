6e86d767e8cdf30902b3fb7eadb5535bc7bb0f97
Readek <readeku@gmail.com>
2021-03-07 20:26:42 +0100

    Second commit

---
+++ site.js
@@ -0,0 +1,25 @@
+// "Check my projects!" click event
+const stBox = document.getElementById("stBox")
+document.getElementById("introDesc").addEventListener("click", () => {
+    stBox.scrollIntoView();
+});
+
+const guiVid = document.getElementById("guiVid");
+//plays and pauses the videos to help with performance, i think
+const vids = document.querySelectorAll(".vid");
+const imageObserver = new IntersectionObserver( (entries, observer) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting) {
+            entry.target.play();
+        } else {
+            entry.target.pause();
+        }
+    });
+});
+vids.forEach( (vid) => {
+    imageObserver.observe(vid);
+});
+
+
+
+  
